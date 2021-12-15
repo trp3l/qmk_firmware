e21f74d4d8bb4b31cc098e3374024bdb103a1ad6
yfuku <fukumoto@gmail.com>
2021-12-15 17:20:39 +0900

    add rev2

--- keyboards/dailycraft/claw44/claw44.h
+++ keyboards/dailycraft/claw44/claw44.h
@@ -3,3 +3,6 @@
 #ifdef KEYBOARD_dailycraft_claw44_rev1
   #include "rev1.h"
 #endif
+#ifdef KEYBOARD_dailycraft_claw44_rev2
+  #include "rev2.h"
+#endif
--- keyboards/dailycraft/claw44/rules.mk
+++ keyboards/dailycraft/claw44/rules.mk
@@ -2,19 +2,22 @@
 MCU = atmega32u4
 
 # Bootloader selection
-BOOTLOADER = caterina
+BOOTLOADER = atmel-dfu
 
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
+COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. 
 
-DEFAULT_FOLDER = dailycraft/claw44/rev1
+SPLIT_KEYBOARD = yes
+ENCODER_ENABLE = yes
+
+DEFAULT_FOLDER = dailycraft/claw44/rev2
