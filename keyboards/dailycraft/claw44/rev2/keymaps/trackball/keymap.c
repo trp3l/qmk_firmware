d04e400f317b3db95039fbc4954829b31128985b
yfuku <fukumoto@gmail.com>
2022-01-30 00:19:36 +0900

    add mouse buttos

--- keyboards/dailycraft/claw44/rev2/keymaps/trackball/keymap.c
+++ keyboards/dailycraft/claw44/rev2/keymaps/trackball/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,--------+--------+--------+--------+--------+--------.                 ,--------+---------+--------+---------+--------+--------.
        KC_ESC , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U    , KC_I   , KC_O    , KC_P   , KC_MINS,
     //|--------+--------+--------+--------+--------+--------|                 |--------+---------+--------+---------+--------+--------|
-       KC_TAB , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_BSPC, KC_BSPC, KC_H   , KC_J    , KC_K   , KC_L    , KC_SCLN, KC_QUOT,
+       KC_TAB , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_MS_BTN1,KC_NO, KC_H   , KC_J    , KC_K   , KC_L    , KC_SCLN, KC_QUOT,
     //|--------+--------+--------+--------+--------+--------|                 |--------+---------+--------+---------+--------+--------|
-       KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_DEL , KC_DEL , KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, KC_RSFT,
+       KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_MS_BTN2,KC_NO, KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, KC_RSFT,
     //`--------+--------+--------+--------+--------+--------/                 \--------+---------+--------+---------+--------+--------'
                         KC_A_DEL, KC_G_EN, KC_L_SPC,KC_C_BS,                   KC_C_BS, KC_R_ENT, KC_G_JA, KC_A_DEL
     //                `----------+--------+--------+--------'                 `--------+---------+--------+---------'
